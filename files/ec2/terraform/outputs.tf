e08e0a1438b8fe79310613350aa766b4fc9bd0a8
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 12:23:03 +0530

    Update outputs.tf

--- files/ec2/terraform/outputs.tf
+++ files/ec2/terraform/outputs.tf
@@ -13,3 +13,23 @@ output "db_user" {
 output "db_password" {
   value = aws_db_instance.db.password
 }
+
+output "alb_dns" {
+  value = aws_lb.app_alb.dns_name
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "cloudfront_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.frontend.bucket
+}
