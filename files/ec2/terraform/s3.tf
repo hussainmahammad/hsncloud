e1beed827b8e49a8c4b080317ac67a79d25d1dad
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 11:43:01 +0530

    Update s3.tf

--- files/ec2/terraform/s3.tf
+++ files/ec2/terraform/s3.tf
@@ -1,3 +1,28 @@
-data "aws_s3_bucket" "frontend" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "frontend" {
+  bucket = "${var.app_name}-frontend-${random_id.bucket_id.hex}"
+
+  tags = {
+    Name = "${var.app_name}-frontend"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls  = false
+  restrict_public_buckets = false
 }
