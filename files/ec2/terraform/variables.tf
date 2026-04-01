6cf273c0f9ff1f418ad922d7160f52c1b66400cb
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 10:46:55 +0530

    Update variables.tf

--- files/ec2/terraform/variables.tf
+++ files/ec2/terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
 
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {}
+
+variable "bucket_name" {}
+
+variable "app_name" {
+  default = "hsncloud"
+}
