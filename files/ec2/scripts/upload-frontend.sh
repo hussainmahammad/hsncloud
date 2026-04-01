33dab7e3186c9114b69b4b3eb4ce5070d783bd05
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 11:03:25 +0530

    Update upload-frontend.sh

--- files/ec2/scripts/upload-frontend.sh
+++ files/ec2/scripts/upload-frontend.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+set -e
+
+echo "🚀 Uploading frontend to S3..."
+
+BUILD_DIR="app/frontend/dist"
+BUCKET_NAME=$1
+
+aws s3 sync $BUILD_DIR s3://$BUCKET_NAME --delete
+
+echo "✅ Frontend uploaded successfully"
