0cc9f1e5d70d6277cd55d52b8a1ee0847007d99a
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 11:04:02 +0530

    Update invalidate-cache.sh

--- files/ec2/scripts/invalidate-cache.sh
+++ files/ec2/scripts/invalidate-cache.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+set -e
+
+echo "🚀 Invalidating CloudFront cache..."
+
+DISTRIBUTION_ID=$1
+
+aws cloudfront create-invalidation \
+  --distribution-id $DISTRIBUTION_ID \
+  --paths "/*"
+
+echo "✅ Cache invalidated" 
