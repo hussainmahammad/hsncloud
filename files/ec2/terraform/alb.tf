42c2c9029c165d35f5f144e3f12498eabd0c50e3
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 10:54:09 +0530

    Update alb.tf

--- files/ec2/terraform/alb.tf
+++ files/ec2/terraform/alb.tf
@@ -1,1 +1,40 @@
+data "aws_vpc" "default" {
+  default = true
+}
 
+data "aws_subnets" "default" {}
+
+resource "aws_lb" "app_alb" {
+  name               = "${var.app_name}-alb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.app_name}-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path = "/api/health"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.app.id
+  port             = 5000
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
