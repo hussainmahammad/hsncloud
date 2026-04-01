11e260f5dc64910af0dcd8acbc15e09e0a67482d
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-01 10:54:47 +0530

    Update cloudfront.tf

--- files/ec2/terraform/cloudfront.tf
+++ files/ec2/terraform/cloudfront.tf
@@ -1,1 +1,41 @@
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = data.aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+  }
 
+  origin {
+    domain_name = aws_lb.app_alb.dns_name
+    origin_id   = "alb-origin"
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/api/*"
+    target_origin_id = "alb-origin"
+
+    allowed_methods = ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"]
+    cached_methods  = ["GET", "HEAD"]
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  enabled = true
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
