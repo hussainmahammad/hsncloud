8ff4b8ce5c07f9b829b7d1d84592d3a183354053
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-04-02 10:41:12 +0530

    Update ec2.tf

--- files/ec2/terraform/ec2.tf
+++ files/ec2/terraform/ec2.tf
@@ -1,17 +1,6 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  owners = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
-  }
-}
-
 resource "aws_instance" "app" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+  ami           = "ami-0ec10929233384c7f"
+  instance_type = "m7i-flex.large"
   key_name      = var.key_name
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
