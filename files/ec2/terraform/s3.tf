307e0abbb8c7561ac8c4997525c6db7930517ac0
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-05-30 17:33:29 +0530

    Update s3.tf

--- files/ec2/terraform/s3.tf
+++ files/ec2/terraform/s3.tf
@@ -21,8 +21,30 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
 resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
-  block_public_acls   = false
-  block_public_policy = false
-  ignore_public_acls  = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_public" {
+  bucket = aws_s3_bucket.frontend.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.frontend
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid       = "PublicReadObjects"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
 }
